dff4bc5c4d4703e1dc8bc76a8de3d29181777865
eapeterson <elizabeth.peterson1229@gmail.com>
2015-07-07 23:48:54 -0400

    First commit

---
+++ js/app.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+	$('.ryu').mouseenter(function() {
+		$('.ryu-still').hide();
+		$('.ryu-ready').show();
+	});
+	$('.ryu').mouseleave(function() {
+		$('.ryu-ready').hide();
+		$('.ryu-still').show();
+	});
+	$('.ryu').mousedown(function() {
+		$('.ryu-ready').hide();
+		$('.ryu-throwing').show();
+		$('.hadouken').show();
+		//play hadouken sound
+		//show hadouken and animate it to the right
+	});
+	$('.ryu').mouseup(function() {
+		$('.ryu-throwing').hide();
+		$('.ryu-ready').show();
+	});
+});
